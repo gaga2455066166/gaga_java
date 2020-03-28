8787821b69d1ee8828e224d3d3982d5aef5cd879
gaga2455066166 <2455066166@qq.com>
2020-03-28 16:59:55 +0800

    20200328_TestGit

--- IDEA测试练习/src/com/cjl/Task3月/TestGit.java
+++ IDEA测试练习/src/com/cjl/Task3月/TestGit.java
@@ -1,4 +1,9 @@
 package com.cjl.Task3月;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
