3397a2bd9d0e8b498a7814d7b17c3f0bc153e7aa
gaga2455066166 <52930591+gaga2455066166@users.noreply.github.com>
2020-10-15 10:40:12 +0800

    jQuery

--- IDEA测试练习/UserTest1/src/com/cjl/web/listener/ContextLoaderListener.java
+++ IDEA测试练习/UserTest1/src/com/cjl/web/listener/ContextLoaderListener.java
@@ -1,6 +1,17 @@
 package com.cjl.web.listener;
 
+import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ContextLoaderListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("在服务器被正常关闭后，这个方法被调用，ServletContext对象被销毁......");
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("服务器启动后，ServletContext对象被创建......");
+    }
 }
